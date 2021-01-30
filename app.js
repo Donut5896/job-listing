90d3c42aaef9c7d3671b1852369bd21b746e7a0e
Donut5896 <Nutchanokchon5@gmail.com>
2021-01-30 20:13:06 +0700

    Done

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const charactersList = document.getElementById('charactersList');
 const searchBar = document.getElementById('searchBar');
-let tags = document.getElementsByClassName("tag");
 let  hpCharacters = [];
 
 
@@ -17,15 +16,6 @@ searchBar.addEventListener('keyup', (e) => {
 });
 
 
-
-
-let theRole, theLevel, theLanguage = [];
-let mainObj = {};
-
-
-
-
-
 const loadCharacters = async () => {
     try{
         const res = await fetch("./data.json");
@@ -38,41 +28,6 @@ const loadCharacters = async () => {
     
 };
 
-const addTags = (data) => {
-   for(let i=0; i<data.length; i++){
-    theRole = data[i].role
-    theLevel = data[i].level
-    theLanguage = data[i].languages
-   //console.log(theRole)
-   }
- 
-}
-
-function myFunction(){
-    //tag = document.createElement("button")
-    //document.tags.createElement = theRole ;
-   
-   //console.log();
-   for(let i=0; i< tags.length; i++){
-
-       // tags[i].style.background = "red";
-
-
- 
- 
-        if(tags[i].onclick === theRole){
-          tags = document.createElement("button")
-          tags.innerHTML = theRole.innerText;
-         searchBar.appendChild(tags);
-         console.log(searchBar);
-        }else if(tags[i] === theLevel){
-         tags.innerHTML = theLevel.innerText;
-         searchBar.appendChild(tags);
-        }
-   }
-}
-
- myFunction();
 
 
 const displayCharacters = (characters) => {
@@ -86,56 +41,24 @@ const displayCharacters = (characters) => {
                     <span >${character.postedAt} ${character.contract} ${character.location}</span>
                 </ul>
                 <div class="tags">
-                      <p class="tag" onclick="myFunction()" data-role="frontend ">${character.role}</p>
-                      <p class="tag" onclick="myFunction()" data-role="frontend ">${character.level}</p>
-                      <p class="tag" onclick="myFunction()" data-role="frontend ">${character.languages}</p>
+                      <p class="tag" >${character.role}</p>
+                      <p class="tag" >${character.level}</p>
+                      <p class="tag"  >${character.languages}</p>
                 </div>
           </li>`;
-      ;
-             
+      
+    
+    
     }).join('')
     charactersList.innerHTML = htmlString;
-  
-     addTags(characters)
+
+
 };
 
+
  loadCharacters();
- 
 
 
-
-
-
-
-
-
-
-
-
-
- //let tags = document.getElementsByClassName("tag");
-
-
-
-
-
-
-
-/*tags.addEventListener('click', (tag) => {
-        const searchTags = tag.target.value
-       let div = document.createElement("p");
-       if(searchTags === "${tag.role}"){
-           div.innerHTML = "${tag.role.value}"
-           searchBar.appendChild(div);
-           console.log(div);
-
-       }else if(searchTags === character.level){
-           div.innerHTML = "${characters.level.value}"
-           searchBar.appendChild(tag);
-           console.log(searchBar);
-       }
- })
-*/
    
- 
+
  
